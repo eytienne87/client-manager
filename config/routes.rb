f887b1d2c2a4efd9a3e6bead488b5a8cccd6877c
Etienne Tessier <etienne_tessier100@hotmail.ca>
2021-11-07 11:49:31 -0500

    added links on index page and show page to create, edit and delete a client

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/clients', to: 'clients#index'
-  get '/clients/new', to: 'clients#new'
+  get '/clients/new', to: 'clients#new', as: :new_client_path
   post '/clients', to: 'clients#create'
   get '/clients/:id', to: 'clients#show', as: :show_client_path
-  get '/clients/:id/edit', to: 'clients#edit'
+  get '/clients/:id/edit', to: 'clients#edit', as: :edit_client_path
   patch '/clients/:id', to: 'clients#update'
   delete '/clients/:id', to: 'clients#destroy', as: :delete_client_path
 end
